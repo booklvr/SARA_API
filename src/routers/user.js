9478f3aa2d4f7ec083886825e78632137b4b9124
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-02-28 20:05:06 +0900

    add routes to add update and delete avatar, add route to update and delete logged in user

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,8 @@ const   express = require('express'),
         auth    = require('../middleware/auth'),
         User    = require('../models/user'),
         multer  = require('multer'), // required for file uploads
-        sharp   = require('sharp'); // convert and resize images
+        sharp   = require('sharp'), // convert and resize images
+        upload  = require('../middleware/avatar.multer');
 
 const router = new express.Router();
 
@@ -87,8 +88,107 @@ router.post('/logoutAll', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send(e);
     }
+});
+
+
+// UPDATE USER
+router.patch('/me', auth, async (req, res) => {
+    // what is allowed to update
+    const updates = Object.keys(req.body) // returns list of keys from req.body
+    console.log(updates);
+    const allowedUpdates = ['name', 'email', 'password'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!' });
+    }
+
+    // use findById for password hashing middleware or mongoose bypasses middleware with findByIdAndUpdate
+    try {
+        //get user from auth middleware req.user
+        // update each field provided by req.body
+        updates.forEach(update => req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/me', auth, async (req, res) => {
+    try {
+        await req.user.remove();
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+// POST AVATAR
+// * upload.single() is middlware provided by multer
+// -> upload.single() requires an arguemnt we are just aclling upload
+// -> 'upload' must match key value in req.body (key value pair in postman)
+router.post('/me/avatar', auth, upload.single('avatar'), async (req, res) => {  // async for req.user.save()
+
+  // * use sharp to resize photo and convert to png format
+  //  -> req.file.buffer is from multer and contains binary info form the image uploaded
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+
+  req.user.avatar = buffer;
+  await req.user.save();  // save file to user profile
+  res.send();
+}, (error, req, res, next) => { // all four arguments needed so express knows to expect an error
+  res.status(400).send({error: error.message }); // error from upload.single multer middleware
 })
 
-//
+// GET AVATAR
+// image available at URL just include in image src in html markup
+router.get('/:id/avatar', async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id);
+
+        if(!user) {
+            throw new Error(); // no error message because not sending error message bellow
+        }
+
+        // create response header
+        // * res.set takes in key value pair { nameOfResponseHeader; valueTryingToSet}
+        res.set('Content-Type', 'image/png'); // we know it is png because we convereted using sharp in postroute
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send();
+    }
+});
+
+// UPDATE AVATAR   ??? DO I EVEN NEED THIS??? IT IS THE SAME AS POST
+router.patch('/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    // async for req.user.save()
+    // * use sharp to resize phto and convert to png format
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+
+    req.user.avatar = buffer;
+    await req.user.save(); // save file to user profile
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+})
+
+
+// DELETE AVATAR
+router.delete('/me/avatar', auth, async (req, res) => {
+
+    try {
+        // remove ausing express remove() and req.user from auth middleware
+        req.user.avatar = undefined; // this deletes binary data from req.user.avatar
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+    }
+});
+
 
 module.exports = router;
