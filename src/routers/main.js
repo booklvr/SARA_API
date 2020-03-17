d8b3c6ea758cefd40cd124e8ad646f27179b3fc5
Nichlas de Waal <nick.a.dewaal@gmail.com>
2020-03-17 14:05:49 +0900

    continue working on frontend working on navar

--- src/routers/main.js
+++ src/routers/main.js
@@ -9,28 +9,28 @@ router.get('/', (req, res) => {
 })
 
 // Get Locations
-router.get('/locations', async (req, res) => {
-    try {
-        const users = await User.find({});
+// router.get('/locations', async (req, res) => {
+//     try {
+//         const users = await User.find({});
 
-        if (!users) {
-            throw new Error("No users");
-        }
+//         if (!users) {
+//             throw new Error("No users");
+//         }
 
-        // const location = [];
-        users.forEach((user) => {
-            // console.log("user", user)
-            console.log(Object.keys(user));
-        })
+//         // const location = [];
+//         users.forEach((user) => {
+//             // console.log("user", user)
+//             console.log(Object.keys(user));
+//         })
         
 
         
 
-        res.status(200).send(users); 
-    } catch(err) {
-        console.log(err);
-    }
-})
+//         res.status(200).send(users); 
+//     } catch(err) {
+//         console.log(err);
+//     }
+// })
 
 router.get('/map', (req, res) => {
     res.render("pages/map", {currentUser: undefined})
